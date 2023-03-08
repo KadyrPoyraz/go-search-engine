4af91181d0f53b2c943ed28f03e02a6db9ef10dd
KadyrPoyraz <kadyrpoyraz@gmail.com>
2023-03-08 16:02:41 +0600

    Restructured main.go file

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"go-search-engine/utils"
 	"io/ioutil"
+	"os"
 	"strings"
 
-	"go-search-engine/utils"
-	"go-search-engine/lexer"
 	"go-search-engine/data"
+	"go-search-engine/lexer"
 )
 
 const (
@@ -20,35 +22,101 @@ var (
 )
 
 func main() {
-	data := data.Data{
-		FileTermFreq: make(map[string]map[string]int),
-		FileTermCount: make(map[string]int),
-	}
-	indexJsonFile, err := ioutil.ReadFile(indexJsonFileName)
+	entry()
 
-	if err != nil {
-		readDir(targetDirectory, data)
-		cacheData(data)
-	}
+	//indexJsonFile, err := ioutil.ReadFile(indexJsonFileName)
+	//
+	//if err != nil {
+	//	readDir(targetDirectory, data)
+	//	cacheData(data)
+	//}
+	//
+	//err = json.Unmarshal(indexJsonFile, &data)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 
-	err = json.Unmarshal(indexJsonFile, &data)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(data)
+	//fmt.Println(data)
 }
 
-func cacheData(data data.Data) {
+func entry() {
+	fmt.Println("Starting the application...")
+	indexCmd := flag.NewFlagSet("index", flag.ExitOnError)
+	indexDirToFilePath := indexCmd.String("dirPath", "", "The path to the dir to be indexed")
+	indexFilePath := indexCmd.String("indexFilePath", "index.json", "Name of file with indexed data")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Expected \"index\" subcommand")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "index":
+		fmt.Println("Index in process...")
+		indexCmd.Parse(os.Args[2:])
+
+		isIndexFileExists := checkForIndexedData(*indexFilePath)
+		if !isIndexFileExists {
+			indexDirToFile(*indexDirToFilePath, *indexfilePath)
+		}
+
+		fmt.Printf("Dir %s has been indexed into file %s\n", *indexDirToFilePath, *indexFilePath)
+	default:
+		fmt.Println("Expected \"index\" subcommand")
+		os.Exit(1)
+	}
+}
+
+func checkForIndexedData(indexFilePath string) bool {
+	_, err := os.Stat(indexFilePath)
+
+	return err == nil
+}
+
+func cacheData(data data.Data, indexFilePath string) {
 	b, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	err = ioutil.WriteFile(indexJsonFileName, b, 0644)
+	err = ioutil.WriteFile(indexFilePath, b, 0644)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func indexDirToFile(dirPath string, indexFilePath string) {
+	data := data.Data{
+		FileTermFreq: make(map[string]map[string]int),
+		FileTermCount: make(map[string]int),
+	}
+
+	collectDirToData(dirPath, data)
+	cacheData(data, indexFilePath)
+}
+
+func collectDirToData(dirPath string, dataStruct data.Data) {
+	items, err := ioutil.ReadDir(dirPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, item := range items {
+		itemPath := dirPath + "/" + item.Name()
+
+		if item.IsDir() {
+			collectDirToData(itemPath, dataStruct)
+			continue
+		}
+		content := utils.GrabTextFromFile(itemPath)
+
+		if len(content) < 1 {
+			continue
+		}
+
+		collectData(dataStruct, itemPath, content)
 	}
 }
 
@@ -61,29 +129,5 @@ func collectData(data data.Data, filePath string, content string) {
 
 		data.AddFileTermFreqItem(filePath, token)
 		data.AddFileTermCount(filePath)
-	}
-}
-
-func readDir(dirPath string, data data.Data) {
-
-	items, err := ioutil.ReadDir(dirPath)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for _, item := range items {
-		itemPath := dirPath + "/" + item.Name()
-
-		if item.IsDir() {
-			readDir(itemPath, data)
-			continue
-		}
-		content := utils.GrabTextFromFile(itemPath)
-
-		if len(content) < 1 {
-			continue
-		}
-		collectData(data, itemPath, content)
 	}
 }

ed7b326554c08479859e8fa7ac0b7902692fb005
KadyrPoyraz <kadyrpoyraz@gmail.com>
2023-03-14 13:43:34 +0600

    Added tests for files package

---
+++ utils/files_test.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"os"
+	"testing"
+)
+
+func getCurrentDirPath() string {
+	path, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return path
+}
+
+type GrabTextFromFileTest struct {
+	name 		  string
+	filepath 	  string
+	expected 	  string
+	expectedError error
+}
+
+var pathToTestFiles = getCurrentDirPath() + "/files_testdata/GrabTextFromFileTest/"
+
+var grabTextFromFileTests = []GrabTextFromFileTest{
+	{
+		name: "Test grabbing from xhtml files",
+		filepath: pathToTestFiles + "testFile.xhtml",
+		expected: "Expected data is: Some test data expected after grabbing text from .xhtml files",
+	},
+	{
+		name: "Test grabbing from txt files",
+		filepath: pathToTestFiles + "testFile.txt",
+		expected: "Expected data is: Some test data expected after grabbing text from .txt files",
+	},
+	{
+		name: "Test grabbing from txt files",
+		filepath: "ErrorPath",
+		expected: "",
+	},
+}
+
+func TestGrabTextFromFile(t *testing.T) {
+	tests := grabTextFromFileTests
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := GrabTextFromFile(test.filepath)
+
+			if test.expected != actual {
+				t.Errorf("FAILED: expected %s, got %s\n", test.expected, actual)
+			}
+		})
+	}
+}
